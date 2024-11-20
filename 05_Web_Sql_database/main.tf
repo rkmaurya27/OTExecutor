80e06c90136735a18ad0442323460ddc755f61b7
Rahul Kr Maurya <rkmaurya2710@gmail.com>
2024-11-20 14:45:29 +0530

    Update main.tf

--- 05_Web_Sql_database/main.tf
+++ 05_Web_Sql_database/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "plan" {
 }
 
 resource "azurerm_linux_web_app" "app" {
-  name                = "mywebapp-01357"
+  name                = "mywebapp-rkm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.plan.location
   service_plan_id     = azurerm_service_plan.plan.id
@@ -48,7 +48,7 @@ resource "azurerm_mssql_server" "sql" {
 }
 
 resource "azurerm_mssql_database" "db" {
-  name           = "ProductsDB"
+  name           = "ProductsDBrkm"
   server_id      = azurerm_mssql_server.sql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
@@ -57,7 +57,7 @@ resource "azurerm_mssql_database" "db" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "tfstorage013"
+  name                     = "tfstorage013rkmnew"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
